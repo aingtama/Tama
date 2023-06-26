1c8f4b10701c4ae32df3e006e169e30ddcdcf92b
aingtama <132202670+aingtama@users.noreply.github.com>
2023-06-27 02:02:32 +0700

    Create Ter.php

---
+++ Ter.php
@@ -0,0 +1,60 @@
+<HTML>
+	<head>
+		<title>infected by S4NGKURl4NG</title>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="description" content="patch your bug sir" />
+		<meta name="keywords" content="S4NGKURl4NG"/>
+		<link rel="icon" type="image/jpg" href="https://i.ibb.co/hYsmfHj/20230519-230535.jpg"/>
+		<link rel="preconnect" href="https://fonts.googleapis.com"/>
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Martian+Mono:wght@300&display=swap" rel="stylesheet"/>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@1,300&display=swap" rel="stylesheet">
+	</head>
+	<body bgcolor="black" onclick="play()">
+		<div class="content" style="top: 0;left: 0;right: 0;bottom: 0;height: 50%;margin: auto;position: absolute;"/>
+		<center>
+			<img alt="LOCALGHOST CLAN" src="https://i.ibb.co/G23PYwm/20230601-224733.jpg" width="400px">
+			<font size="4px" face="Martian Mono:wght" color="white"><h1>infected by <font color="red">S4NGKURl4NG</font><br><br>
+            <font size="4px" face="Martian Mono:wght" color="white"><br>hey admin,your site is too weak that i can infiltrate and break its security system from inside</font><br></h1></font>
+			<font face="Courier New" size="5" color="white">[ INDONESIAN HACKER RULES ]</font></br></br>
+<audio id="audioNotifikasi">
+
+  <source src="https://github.com/randixploit/hek/raw/main/yt5s.com%20-%20somewhere%20only%20we%20know%20(Gustixa%20&%20Rhianne)%20(128%20kbps).mp3" type="audio/mpeg">
+
+</audio>
+
+<button style="border:2px solid red; border-radius:30px; background-color:black;" onclick="playAudio()" type="button"><font face="monaco" size="3" color="white">Play Music</font></button>
+
+<button style="border:2px solid red; border-radius:30px; background-color:black;" onclick="pauseAudio()" type="button"><font face="monaco" size="3" color="white">Pause Music</font></button> <br>
+
+<a href="https://www.facebook.com/profile.php?id=100092463732483"><button style="border:2px solid red; border-radius:30px; background-color:black;"><font face="monaco" size="3" color="white">Contact Me</font></button></a>
+
+<script type="text/javascript">
+
+var el = document.getElementById("audioNotifikasi"); 
+
+function playAudio() { 
+
+  el.play(); 
+
+} 
+
+function pauseAudio() { 
+
+  el.pause(); 
+
+} 
+
+</script>
+
+<br>
+		</center>
+		</div>
+	</body>
+	  <script>
+    var x = document.getElementById("sec");
+    function play() {
+      x.play(); 	
+    }
+  </script>
+</html>
