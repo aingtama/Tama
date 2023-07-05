0b8c93856b2a5a8bcc6fb9b960b47b76441ba7f7
aingtama <132202670+aingtama@users.noreply.github.com>
2023-07-05 15:44:09 +0700

    Update Ter.php

--- Ter.php
+++ Ter.php
@@ -1,9 +1,9 @@
 <HTML>
 	<head>
-		<title>infected by S4NGKURl4NG</title>
+		<title>infected by Takashi</title>
 		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 		<meta name="description" content="patch your bug sir" />
-		<meta name="keywords" content="S4NGKURl4NG"/>
+		<meta name="keywords" content="Takashi"/>
 		<link rel="icon" type="image/jpg" href="https://i.ibb.co/hYsmfHj/20230519-230535.jpg"/>
 		<link rel="preconnect" href="https://fonts.googleapis.com"/>
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,7 +14,7 @@
 		<div class="content" style="top: 0;left: 0;right: 0;bottom: 0;height: 50%;margin: auto;position: absolute;"/>
 		<center>
 			<img alt="LOCALGHOST CLAN" src="https://i.ibb.co/G23PYwm/20230601-224733.jpg" width="400px">
-			<font size="4px" face="Martian Mono:wght" color="white"><h1>infected by <font color="red">S4NGKURl4NG</font><br><br>
+			<font size="4px" face="Martian Mono:wght" color="white"><h1>infected by <font color="red">Takashi</font><br><br>
             <font size="4px" face="Martian Mono:wght" color="white"><br>hey admin,your site is too weak that i can infiltrate and break its security system from inside</font><br></h1></font>
 			<font face="Courier New" size="5" color="white">[ INDONESIAN HACKER RULES ]</font></br></br>
 <audio id="audioNotifikasi">
